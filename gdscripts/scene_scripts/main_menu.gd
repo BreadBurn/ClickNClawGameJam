1619a5941f958324ed9bed0b633bacb33d85058d
wish86 <mail.chromaky@gmail.com>
2026-03-15 11:51:42 +1100

    added level for testing

--- gdscripts/scene_scripts/main_menu.gd
+++ gdscripts/scene_scripts/main_menu.gd
@@ -1,4 +1,6 @@
 extends Control
 
 func _on_start_pressed() -> void:
-	get_tree().change_scene_to_file("res://scenes/game_main.tscn")
+	# get_tree().change_scene_to_file("res://scenes/game_main.tscn")
+	# going to the level scene now!
+	get_tree().change_scene_to_file("res://scenes/level.tscn")
